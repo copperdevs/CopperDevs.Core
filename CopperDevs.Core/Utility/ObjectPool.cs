b901cf6821d5beb3cea10003d359e0fe5d0925a2
copperdevs <github@copperdevs.com>
2025-05-11 13:54:45 -0500

    object pool cleanup and optional

--- CopperDevs.Core/Utility/ObjectPool.cs
+++ CopperDevs.Core/Utility/ObjectPool.cs
@@ -1,23 +1,21 @@
-using System.Reflection;
-
 namespace CopperDevs.Core.Utility;
 
 /// <summary>
 /// Simple object pool for types
 /// </summary>
 /// <typeparam name="T">Type of object to pool</typeparam>
-public static class ObjectPool<T> where T : IObjectPoolable, new()
+public class ObjectPool<T> where T : IObjectPoolable, new()
 {
-    private static readonly Stack<T> Pool = new();
+    private readonly Stack<T> pool = new();
 
     /// <summary>
     /// Get an object from the pool if one is available, otherwise it creates a new one
     /// </summary>
     /// <returns>The object from the pool</returns>
-    public static T Get()
+    public T Get()
     {
-        var obj = Pool.Count > 0 ? Pool.Pop() : new T();
-        PoolableUpdate(obj, PoolableUpdateType.Gotten);
+        var obj = pool.Count > 0 ? pool.Pop() : new T();
+        obj.Gotten();
         return obj;
     }
 
@@ -25,33 +23,9 @@ public static class ObjectPool<T> where T : IObjectPoolable, new()
     /// Returns an object to the pool to be reused
     /// </summary>
     /// <param name="obj">Object to return</param>
-    public static void Return(T obj)
+    public void Return(T obj)
     {
-        PoolableUpdate(obj, PoolableUpdateType.Released);
-        Pool.Push(obj);
-    }
-
-    private static void PoolableUpdate(T obj, PoolableUpdateType updateType)
-    {
-        if (!typeof(T).Implements<IObjectPoolable>())
-            return;
-
-        switch (updateType)
-        {
-            case PoolableUpdateType.Gotten:
-                obj.Gotten();
-                break;
-            case PoolableUpdateType.Released:
-                obj.Released();
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(updateType), updateType, null);
-        }
-    }
-
-    private enum PoolableUpdateType
-    {
-        Gotten,
-        Released
+        obj.Released();
+        pool.Push(obj);
     }
 }
---
+++ CopperDevs.Core/Utility/Optional.cs
@@ -0,0 +1,68 @@
+namespace CopperDevs.Core.Utility;
+
+/// <summary>
+/// Represents an optional value that may or may not be set.
+/// </summary>
+/// <typeparam name="TValue">The type of the value.</typeparam>
+public readonly struct Optional<TValue>
+{
+    /// <summary>
+    /// Gets a value indicating whether this <see cref="Optional{TValue}"/> contains a value.
+    /// </summary>
+    public bool Enabled { get; }
+
+    /// <summary>
+    /// Gets the value
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if <see cref="Enabled"/> is false.</exception>
+    public TValue Value
+    {
+        get
+        {
+            if (!Enabled)
+                throw new InvalidOperationException("Optional has no value.");
+            return value!;
+        }
+    }
+
+    private readonly TValue? value;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Optional{TValue}"/> struct with the specified value.
+    /// </summary>
+    /// <param name="value">The value to wrap.</param>
+    public Optional(TValue value)
+    {
+        Enabled = true;
+        this.value = value;
+    }
+
+    private Optional(bool enabled, TValue? value)
+    {
+        Enabled = enabled;
+        this.value = value;
+    }
+
+    /// <summary>
+    /// Returns an empty <see cref="Optional{TValue}"/> instance.
+    /// </summary>
+    public static Optional<TValue> None => new(false, default);
+
+    /// <summary>
+    /// Implicitly wraps a value into an <see cref="Optional{TValue}"/>.
+    /// </summary>
+    /// <param name="value">The value to wrap.</param>
+    public static implicit operator Optional<TValue>(TValue value) => new(value);
+
+    /// <summary>
+    /// Attempts to get the value.
+    /// </summary>
+    /// <param name="value">The output value if present.</param>
+    /// <returns>True if the value is present; otherwise, false.</returns>
+    // ReSharper disable once ParameterHidesMember
+    public bool TryGetValue(out TValue? value)
+    {
+        value = this.value;
+        return Enabled;
+    }
+}
